bb6e4903604e6ad599d8b3d1b0547c655f7528a5
rawer <rawer.dev@gmail.com>
2017-11-08 15:23:46 +0800

    MOD : opt compile

--- app/src/main/java/com/ovwvwvo/libarytest/activity/MainActivity.java
+++ app/src/main/java/com/ovwvwvo/libarytest/activity/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        showProgressDialog();
+        showProgressDialog();
     }
 
     @Override
