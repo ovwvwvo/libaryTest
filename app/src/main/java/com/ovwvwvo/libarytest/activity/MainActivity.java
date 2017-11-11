d7d39c00cfabf7b79b336f5fc95642a59d7df25e
rawer <rawer.dev@gmail.com>
2017-11-11 23:54:39 +0800

    upgrade project

--- app/src/main/java/com/ovwvwvo/libarytest/activity/MainActivity.java
+++ app/src/main/java/com/ovwvwvo/libarytest/activity/MainActivity.java
@@ -1,16 +1,8 @@
-/*
- *
- *  * Created by rawer.
- *
- */
-
 package com.ovwvwvo.libarytest.activity;
 
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -20,12 +12,11 @@ import com.ovwvwvo.libarytest.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.ovwvwvo.libarytest.R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         showProgressDialog();
@@ -44,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static Bitmap convertViewToBitmap(View view) {
-        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
         view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
         view.buildDrawingCache();
         return view.getDrawingCache();
